16b46d66b0ffa90099b90b9d7002a4490d54c31e
Volodymyr <113825461+IMorkovkaI@users.noreply.github.com>
2023-08-29 10:01:59 +0200

    sonar-sweep.js

--- sonar-sweep/sonar-sweep.js
+++ sonar-sweep/sonar-sweep.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const input = fs.readFileSync('./input.txt', 'utf-8').split('\n').map(Number);
 
 let count = 0;
-const findIndex = (input) => { 
+const sonarSweep1 = (input) => { 
     for (let i = 0; i < input.length; i++) {
           if(input[i] < input[i+1]) {
             count++;
@@ -11,6 +11,13 @@ const findIndex = (input) => {
       }
       return count;
     }
-    
-findIndex(input);
-console.log(count);
+
+const sonarSweep2 = (input) => {
+  for (let i = 1; i < input.length-2; i++) {
+    let cur = input[i-1] + input[i] + input[i+1];
+    let next = input[i] + input[i+1] + input[i+2];
+    if(cur < next) {
+      count ++;
+    }
+  }
+}
